5cf3600cc931fbb8e9e943401c8d1057cbf146ca
YeeHsun <33437579+YeeHsun@users.noreply.github.com>
2021-05-31 14:33:44 +0800

    Add files via upload

--- client.c
+++ client.c
@@ -1,4 +1,16 @@
-#include"lib.h"
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h>
+#include <sys/time.h>
+#include <pthread.h> 
+#include <semaphore.h> 
+#include <unistd.h>
+#include <arpa/inet.h>
+#define MAX 100 
 
 //global variable
 char user_name[MAX];
